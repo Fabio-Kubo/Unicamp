2db01985d4c02d66bdcff3af9a4d3aca3298ecb7
Fabio Tadashi Kaneiwa Kubo <ra145979@ribeiro.lab.ic.unicamp.br>
2014-04-01 21:17:29 -0300

    Inicio da implementação das interfaces

---
+++ MC302/ProPlan4/src/prodPlan/Factory.java
@@ -0,0 +1,13 @@
+package prodPlan;
+
+import xserial.*;
+
+public class Factory implements XFactory {
+
+	@Override
+	public XSerial makeObject(String name, XSerial upperObj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
