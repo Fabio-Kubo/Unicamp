58c223d574725fbf946aa611707a355bd545810e
Fabio-Kubo <fabiotk.kubo@gmail.com>
2014-05-25 12:48:33 -0300

    Solucao do problema do dict

--- MC302/lab8/distributionCenter.py
+++ MC302/lab8/distributionCenter.py
@@ -1,5 +1,5 @@
 #Fabio Tadashi Kaneiwa Kubo 145979
-import re
+import re, sys
 
 class Cidade(object):
 	def __init__(self, id, capacidade, nome):
@@ -15,16 +15,16 @@ class Distancia(object):
 		self.distancia = distancia
 
 def carregaCidades(arqEntrada):
-	cidades = {}
+	cidades = []
 	qtdCidade = int(arqEntrada.readline())
 	for i in range(0, qtdCidade):
 		linha = arqEntrada.readline()
 		listAux = re.findall('[^\s]+', linha)
-		cidades[listAux[0]] = Cidade(listAux[0], listAux[1], listAux[2])
+		cidades.append(Cidade(listAux[0], listAux[1], listAux[2]))
 	return cidades
 
 def carregaDistancias(arqEntrada, qtdCidade): 
-	matrizDistancias = [[[10000 for k in range(0, qtdCidade + 1)] for j in range(0, qtdCidade + 1)] for i in range(0, qtdCidade + 1)]
+	matrizDistancias = [[[99 for k in range(0, qtdCidade + 1)] for j in range(0, qtdCidade + 1)] for i in range(0, qtdCidade + 1)]
 	pararLeitura = False;
 	arqEntrada.readline(); # ignora o -1-1 xx
 	
@@ -36,6 +36,7 @@ def carregaDistancias(arqEntrada, qtdCidade):
 			pararLeitura = True
 		else:
 			matrizDistancias[0][int(listAux[0])][int(listAux[1])] = int(listAux[2])
+			matrizDistancias[0][int(listAux[1])][int(listAux[0])] = int(listAux[2])
 
 	return matrizDistancias
 
@@ -44,36 +45,37 @@ def calculaMenorCaminho(dist, qtdCidade):
 		for i in range(1, qtdCidade):
 			for j in range(1, qtdCidade):
 				dist[k][i][j] = min(dist[k-1][i][j], dist[k-1][i][k] + dist[k-1][k][j])
+	imprimeMatriz(dist)
 
 def calculaCustoDistribuicao(cidades, matrizDistancias):
 	for cidade in cidades:
 		for cidadeB in cidades:
-			if cidade != cidadeB:
-				cidade.custoDistribuicao = matrizDistancias[len(cidades)][cidade.id][cidadeB.id] * cidadeB.capacidade
-
-def imprimeMatriz(matriz):
-	n = 31
-	for k in range(0, n):
-		for i in range(0, n):
-			for j in range(0, n): 
-				print matriz[k][j][i],
-		print ' '
+			if cidade.id != cidadeB.id:
+				cidade.custoDistribuicao = matrizDistancias[len(cidades)][int(cidade.id)][int(cidadeB.id)] * cidadeB.capacidade
 
 def imprimiResultado(cidades):
-
 	for cidade in cidades:
 		print '[' + cidade.nome + ', '+ cidade.capacidade + ', ' + cidade.custoDistribuicao + ']'
-	print 'melhor ponto: ' + 'falta' + ' custo: ' + 80040
-	print 'maior custo: ' + 'falta' + ' custo: ' + 235413
+	
+	cidades.sort(key=lambda x: x.custoDistribuicao)
+	print 'melhor ponto: ' + cidades[0].nome + ' custo: ' + cidades[0].custoDistribuicao
+	print 'maior custo: ' + cidades[len(cidades) - 1].nome + ' custo: ' + cidades[len(cidades) - 1].custoDistribuicao
 
 def main():
-
-	cidades = {}
+	cidades = []
 	arqEntrada = open('distancias.txt')
 	cidades = carregaCidades(arqEntrada) #monta dicionario que guarda as cidades (chave = id)
 	matrizDistancias = carregaDistancias(arqEntrada, len(cidades)) # carrega as distancias
 	calculaMenorCaminho(matrizDistancias, len(cidades))
-	calculaCustoDistribuicao(cidades, matrizDistancias)
-	imprimiResultado(cidades)
+	#calculaCustoDistribuicao(cidades, matrizDistancias)
+	#imprimiResultado(cidades)
+
+def imprimeMatriz(matriz):
+	n = 31
+	for k in range(0,1):
+		for i in range(0, n):
+			for j in range(0, n):
+				print matriz[n][j][i],
+			print ' '
 
 main()
